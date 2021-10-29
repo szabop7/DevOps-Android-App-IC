478e47a80dd98061922708418af4b06de4e262ec
Alicia Retamosa <a.retamosa@alumnos.upm.es>
2021-10-29 12:37:50 +0200

    Added login funcionality

--- app/src/main/java/com/example/devops/MainActivity.kt
+++ app/src/main/java/com/example/devops/MainActivity.kt
@@ -9,11 +9,18 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.example.devops.databinding.ActivityMainBinding
+import com.auth0.android.Auth0
+import com.auth0.android.authentication.AuthenticationException
+import com.auth0.android.provider.WebAuthProvider
+import com.auth0.android.result.Credentials
+import com.auth0.android.callback.Callback
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var account: Auth0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +31,37 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
         NavigationUI.setupWithNavController(binding.navView,navController)
+
+        account = Auth0(
+            "fFPxEdQJbyPirdQcuzrSNuYiz7tp8nLL",
+            "dev-g6aj--a8.us.auth0.com"
+        )
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.navHostFragment)
         return NavigationUI.navigateUp(navController,binding.drawerLayout)
+    }
+
+    private fun loginWithBrowser() {
+        // Setup the WebAuthProvider, using the custom scheme and scope.
+
+        WebAuthProvider.login(account)
+            .withScheme("demo")
+            .withScope("openid profile email")
+            // Launch the authentication passing the callback where the results will be received
+            .start(this, object : Callback<Credentials, AuthenticationException> {
+                // Called when there is an authentication failure
+                override fun onFailure(exception: AuthenticationException) {
+                    // Something went wrong!
+                }
+
+                // Called when authentication completed successfully
+                override fun onSuccess(credentials: Credentials) {
+                    // Get the access token from the credentials object.
+                    // This can be used to call APIs
+                    val accessToken = credentials.accessToken
+                }
+            })
     }
 }
